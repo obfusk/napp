02f6e12bdf9e16eaa817d8adada3695a065ceb37
Felix C. Stegerman <flx@obfusk.net>
2013-07-23 16:24:03 +0200

    compact code

--- lib/napp/vcss/git.rb
+++ lib/napp/vcss/git.rb
@@ -2,7 +2,7 @@
 #
 # File        : napp/vcss/git.rb
 # Maintainer  : Felix C. Stegerman <flx@obfusk.net>
-# Date        : 2013-07-12
+# Date        : 2013-07-23
 #
 # Copyright   : Copyright (C) 2013  Felix C. Stegerman
 # Licence     : GPLv2
@@ -22,23 +22,17 @@ module Napp; module VCSs; module Git
 
   # clone repo to dir (and checkout branch)
   def self.clone(repo, dir, branch)
-    OU.chk_exit(CLONE[repo, dir, branch]) do |a|
-      OU.ospawn_w *a
-    end
+    OU.chk_exit(CLONE[repo, dir, branch]) { |a| OU.ospawn_w *a }
   end
 
   # pull branch in dir
   def self.pull(dir, branch)
-    OU.chk_exit(PULL[branch]) do |a|
-      OU.ospawn_w *a, chdir: dir
-    end
+    OU.chk_exit(PULL[branch]) { |a| OU.ospawn_w *a, chdir: dir }
   end
 
   # show log in dir, n lines, optionally verbose, newest last
   def self.log(dir, n, verbose)
-    OU.chk_exit(LOG[n,verbose]) do |a|
-      OU.ospawn_w *a, chdir: dir
-    end
+    OU.chk_exit(LOG[n,verbose]) { |a| OU.ospawn_w *a, chdir: dir }
   end
 
 end; end; end
